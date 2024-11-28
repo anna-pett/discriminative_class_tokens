67b04295e853a1a07eb4f902d3fc54ee83073084
anna-pett <93157544+anna-pett@users.noreply.github.com>
2024-11-28 23:59:12 +0200

    ControlNet with Stable Diffusion XL

--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ from diffusers import (
     PNDMScheduler,
     StableDiffusionPipeline,
     UNet2DConditionModel,
+    AutoPipelineForText2Image,
+    StableDiffusionXLControlNetPipeline,
+    ControlNetModel
 )
 from transformers import CLIPTextModel, CLIPTokenizer
 import torchvision.transforms as T
@@ -85,10 +88,21 @@ def prepare_stable(config):
     text_encoder = CLIPTextModel.from_pretrained(
         pretrained_model_name_or_path, subfolder="text_encoder"
     )
-    vae = AutoencoderKL.from_pretrained(pretrained_model_name_or_path, subfolder="vae")
-    pipe = StableDiffusionPipeline.from_pretrained(pretrained_model_name_or_path).to(
-        "cuda"
-    )
+    if config.is_controlnet:
+        # initialize the models and pipeline
+        controlnet = ControlNetModel.from_pretrained(
+            "diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float32
+        ).to(device)
+        # vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float32)
+        pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+            "stabilityai/sdxl-turbo", controlnet=controlnet, torch_dtype=torch.float32, num_inference_steps=config.diffusion_steps
+        ).to(device)
+        pipe.enable_model_cpu_offload()
+    else:
+        vae = AutoencoderKL.from_pretrained(pretrained_model_name_or_path, subfolder="vae")
+        pipe = StableDiffusionPipeline.from_pretrained(pretrained_model_name_or_path).to(
+            "cuda"
+        )
     scheduler = pipe.scheduler
     del pipe
     tokenizer = CLIPTokenizer.from_pretrained(
@@ -106,3 +120,17 @@ def save_progress(text_encoder, placeholder_token_id, accelerator, config, save_
     )
     learned_embeds_dict = {config.placeholder_token: learned_embeds.detach().cpu()}
     torch.save(learned_embeds_dict, save_path)
+
+
+# get canny image for ControlNet
+def load_conditioning_image(config):
+    # image = np.asarray(PIL.Image.open(f"controlnet/{config.amount}_face.png"))
+    if not os.path.exists("controlnet/conditioning_face.png"):
+        raise FileNotFoundError(f"Conditioning image file not found: controlnet/conditioning_face.png")
+    # from https://faces.mpdl.mpg.de/imeji/
+    image = np.asarray(PIL.Image.open(f"controlnet/conditioning_face.png"))
+    image = cv2.Canny(image, 100, 200)
+    image = image[:, :, None]
+    image = np.concatenate([image, image, image], axis=2)
+    canny_image = Image.fromarray(image)
+    
